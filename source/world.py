9882a861da850b945d0583408ee3b82aa833c86f
Joshua Beitler <joshbeitler@gmail.com>
2014-01-29 08:26:10 -0700

    Cleaned up the world.py file, with a new BackgroundLayer class to simplify the updating and drawing of layers, reducing the code by almost three times the lines of code.  Its super simple and awesome.

--- source/world.py
+++ source/world.py
@@ -16,6 +16,29 @@ from sound import Sound, Music
 #
 # -joshbeitler
 
+class BackgroundLayer:
+    def __init__(self, filename, pos, scroll_speed):
+        self.s1 = Sprite(filename, pos)
+        self.s2 = Sprite(filename, (1280, pos[1]))
+
+        self.filename = filename
+        self.position = pos
+        self.speed    = scroll_speed
+
+    def draw(self):
+        self.s1.draw()
+        self.s2.draw()
+
+    def update(self):
+        self.s1.rect.x -= self.speed
+        self.s2.rect.x -= self.speed
+
+        if (self.s1.rect.x + 1280) == 0:
+            self.s1.rect.x = 1280
+        if (self.s2.rect.x + 1280) == 0:
+            self.s1.rect.x = 0
+            self.s2.rect.x = 0
+
 class World:
     """
     World: encapsulates everything related to the world: time, children,
@@ -35,71 +58,30 @@ class World:
         Load the required resources to display the world
         """
 
-        self.bg1 = Sprite("world/test_bg_1.png", (0, 0))
-        self.bg2 = Sprite("world/test_bg_1.png", (1280, 0))
-        
-        self.bg3 = Sprite("world/test_bg_2.png", (0, 0))
-        self.bg4 = Sprite("world/test_bg_2.png", (1280, 0))
+        # The order here matters! Back to front.
+        self.layers = [
+            BackgroundLayer("world/test_bg_3.png", (0, 0), 2.5),
+            BackgroundLayer("world/test_bg_2.png", (0, 0), 5.5),
+            BackgroundLayer("world/test_bg_1.png", (0, 0), 3)
+        ]
 
-        self.bg5 = Sprite("world/test_bg_3.png", (0, 0))
-        self.bg6 = Sprite("world/test_bg_3.png", (1280, 0))
-
-        self.ground1 = Sprite("world/test_bg_4.png", (0, 0))
+        self.ground = Sprite("world/test_bg_4.png", (0, 0))
 
     def update(self):    
         """
         Update critical world information, such as background position, etc.
         """
 
-        # Background layer 1 (light mountains)
-        self.bg1.rect.x -= (self.scroll_factor / 3)
-        self.bg2.rect.x -= (self.scroll_factor / 3)
-
-        if (self.bg1.rect.x + 1280) == 0:
-            self.bg1.rect.x = 1280
-
-        if (self.bg2.rect.x + 1280) == 0:
-            self.bg1.rect.x = 0
-            self.bg2.rect.x = 1280
-
-        # Background layer 2 (clouds)
-        self.bg3.rect.x -= (self.scroll_factor / 2)
-        self.bg4.rect.x -= (self.scroll_factor / 2)
-
-        if (self.bg3.rect.x + 1280) == 0:
-            self.bg3.rect.x = 1280
-
-        if (self.bg4.rect.x + 1280) == 0:
-            self.bg3.rect.x = 0
-            self.bg4.rect.x = 1280
-
-        # Background layer 3 (dark mountains)
-        self.bg5.rect.x -= (self.scroll_factor / 4)
-        self.bg6.rect.x -= (self.scroll_factor / 4)
-
-        if (self.bg5.rect.x + 1280) == 0:
-            self.bg5.rect.x = 1280
-
-        if (self.bg6.rect.x + 1280) == 0:
-            self.bg5.rect.x = 0
-            self.bg6.rect.x = 1280
+        for layer in self.layers:
+            layer.update()
 
     def draw(self):
         """
         Draw the world and everything in it
         """
 
-        # Background layer 3
-        self.bg5.draw()
-        self.bg6.draw()
-
-        # Background layer 1
-        self.bg1.draw()
-        self.bg2.draw()
-        
-        # Background layer 2
-        self.bg3.draw()
-        self.bg4.draw()
+        for layer in self.layers:
+            layer.draw()
 
         # Ground
-        self.ground1.draw()
+        self.ground.draw()
